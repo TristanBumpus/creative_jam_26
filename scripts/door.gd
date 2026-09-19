56ca202e164449a155922b9975adf2c6dfd60615
TristanBumpus <e2467563@cmaisonneuve.qc.ca>
2026-09-19 08:43:11 -0400

    test

--- scripts/door.gd
+++ scripts/door.gd
@@ -1,8 +1,6 @@
 extends StaticBody2D
 
 var locked = false
-var width = 512
-var height = 352
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
--- scripts/level_change.gd
+++ scripts/level_change.gd
@@ -1,5 +1,7 @@
 extends Node2D
 
+var width = 512
+var height = 352
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -16,3 +18,5 @@ func _on_area_2d_area_entered(area: Area2D) -> void:
 		var level = load("res://levels/level_1.tscn").instantiate()
 		
 		get_tree().current_scene.add_child(level)
+		
+		level.global_position = global_position + Vector2(width/2,height/2)

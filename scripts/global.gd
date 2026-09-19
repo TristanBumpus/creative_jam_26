e90d16677b8f82d0d50fede5d94650e3a1be7e4c
TristanBumpus <e2467563@cmaisonneuve.qc.ca>
2026-09-19 19:43:18 -0400

    ui changes and bug fixes

--- scripts/global.gd
+++ scripts/global.gd
@@ -7,10 +7,10 @@ var all_upgrades = [
 	{"name": "Attack Speed up", "desc": "Increases attack speed by ", "effect": 2, "lvl mult": 1.5 },
 	{"name": "Speed up", "desc": "Increases speed by ", "effect": 2, "lvl mult": 2 },
 	{"name": "Dodge time", "desc": "Increases Dodge Time by ", "effect": .2, "lvl mult": .1 },
-	{"name": "Fire Ball", "desc": "Cast explosive Fire Balls ", "effect": 1, "lvl mult": .1 },
+	{"name": "Fire Ball", "desc": "Adds an explosive Fire Balls to your cast ", "effect": 0, "lvl mult": .1 },
 	{"name": "Max health up", "desc": "Increases Maximum health by ", "effect": 5, "lvl mult": .5},
-	{"name": "Ice Ball", "desc": "Cast a ball of Ice ", "effect": 1, "lvl mult": .1 },
-	{"name": "Toxic Ball", "desc": "Cast a Toxic glob ", "effect": 1, "lvl mult": .1 }
+	{"name": "Ice Ball", "desc": "Adds a ball of Ice to  your cast", "effect": 0, "lvl mult": .1 },
+	{"name": "Posion Ball", "desc": "Adds a Toxic glob to your cast ", "effect": 0, "lvl mult": .1 }
 	
 	]
 
@@ -19,7 +19,7 @@ enum effects {
 	slow,
 }
 
-var loot_table = {"commun" : [0,1,2,4], "rare" : [3, 5], "epic" : [4]}
+var loot_table = {"commun" : [0,1,2,5], "rare" : [3,4,6,7]}
 
 var loot_pool = []
 var enemy_table = ["res://entities/enemies/enemy_charger.tscn","res://entities/enemies/enemy_chaser.tscn"]
@@ -65,20 +65,17 @@ func _ready() -> void:
 			#for i2 in range(2):
 		var r = seed.randi_range(1,100)
 		var rarity = "commun"
-		if r <= 50:
+		if r <= 60:
 			rarity = "commun"
-		elif r > 50 and r <= 95:
-			rarity = "rare"
 		else:
-			rarity = "epic" 
-		items += [loot_table[rarity].pick_random()]
+			rarity = "rare"
 			#if items[0] != items[1]:
 				#break
 			#else:
 				#items = []
 		
 		# [level (wave) equivalent, corresponding upgrade (item)]
-		loot_pool += [[i] + items]
+		loot_pool += [[i,  loot_table[rarity].pick_random()]]
 	print(loot_pool)
 
 

0f5c293e54cc06ceba83b7870f976a7b2beb62e8
Firefox13590 <pharlyb@gmail.com>
2026-09-18 21:25:54 -0400

    player direction

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,6 +1,8 @@
 extends Node2D
 
 var player_movement: Vector2
+var player_direction: Vector2
+var mouse_position: Vector2
 @export var player_speed: float = 1
 
 # Called when the node enters the scene tree for the first time.
@@ -10,8 +12,18 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
+#	get input for movement
 	player_movement = Input.get_vector("a", "d", "s", "w")
 	player_movement.y *= -1
+	
+#	get player direction through mouse position
+	mouse_position = get_viewport().get_mouse_position()
+	#print(mouse_position)
+	
+#	apply movement to player
 	$".".translate(player_movement * player_speed)
 	#print(player_movement)
+	
+#	apply direction to player
+	$".".look_at(mouse_position)
 	pass
